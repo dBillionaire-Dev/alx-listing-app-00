18a1731bf9fdd26a799579196656d8372d560456
dBillioniare-Dev <nezerekunke@gmail.cm>
2025-06-01 14:20:11 +0100

    Initial commit

---
+++ constants/index.ts
@@ -0,0 +1,5 @@
+export const API_URL = 'https://api.example.com/properties'
+
+export const APP_NAME = 'ALX Listing App'
+export const APP_DESCRIPTION = 'A simple property listing application built with React and TypeScript.'
+export const APP_VERSION = '1.0.0'
